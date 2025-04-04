5d3ecf2e50477a38db052e438913ed0d7ba9c017
surrim <root@surrim.org>
2025-04-05 01:08:11 +0200

    about page added

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -5,4 +5,9 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     get root_url
     assert_response :success
   end
+
+  test "should get about" do
+    get home_about_url
+    assert_response :success
+  end
 end
